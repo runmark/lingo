0983923eee4e9216b23b7d085c242256942f28d2
maxingmin <ma_xingmin@163.com>
2024-03-28 13:21:16 +0000

    ✨ feat(header): header icon login/out

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button size="lg" variant="default">Click me</Button>
+    <div className="max-w-[988px] w-full mx-auto flex-1 flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero"></Image>
+      </div>
+      <div></div>
+    </div>
   );
 }
