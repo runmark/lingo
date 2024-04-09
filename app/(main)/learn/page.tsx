3e459f9c943dd2ab2478ca0812d052437f01006b
maxingmin <ma_xingmin@163.com>
2024-04-09 09:23:24 +0000

    ✨ feat(learn page): loading

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,17 @@ import FeedWrapper from "@/components/feed-wrapper";
 import StickyWrapper from "@/components/sticky-wrappper";
 import Header from "./header";
 import UserProgress from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+
+    const userProgress = await getUserProgress();
+
+    if (!userProgress?.activeCourseId) {
+        redirect("/courses");
+    }
+
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
--- db/queries.ts
+++ db/queries.ts
@@ -5,16 +5,14 @@ import db from "@/db/drizzle";
 import { eq } from "drizzle-orm";
 import { userProgress } from "@/db/schema";
 
-
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
     return data;
 });
 
 export const getUserProgress = cache(async () => {
-    const { userId } = auth();
+    const { userId } = await auth();
     console.log("userId: ", userId);
-
 
     if (!userId) {
         return null;
@@ -26,6 +24,7 @@ export const getUserProgress = cache(async () => {
             activeCourse: true,
         },
     });
+    console.log(data);
 
     return data;
 });
