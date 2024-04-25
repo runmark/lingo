08837b04088c6620a6440b05b65dc5621601f8c6
maxingmin <ma_xingmin@163.com>
2024-04-25 08:06:34 +0000

    ✨ feat(product-table): reverse data flow

--- app/react-learn/product-table/page.jsx
+++ app/react-learn/product-table/page.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 
 const ProductCategoryRow = ({ catetory }) => {
     return (
@@ -24,12 +27,25 @@ const ProductRow = ({ product }) => {
 };
 
 
-const ProductTable = ({ products }) => {
+const ProductTable = ({ products, filterText, inStockOnly }) => {
 
     const rows = [];
     let lastCategory = null;
 
     products.forEach((product) => {
+
+        if (
+            product.name.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) === -1
+        ) {
+            return;
+        }
+
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
+
         if (lastCategory !== product.category) {
             rows.push(
                 <ProductCategoryRow
@@ -38,6 +54,18 @@ const ProductTable = ({ products }) => {
                 />
             );
         }
+
+        // if (
+        //     (!filterText || product.name.contains(filterText))
+        //     && (!inStockOnly || product.stocked)
+        // ) {
+        //     rows.push(
+        //         <ProductRow
+        //             key={product.name}
+        //             product={product}
+        //         />
+        //     );
+        // }
 
         rows.push(
             <ProductRow
@@ -62,11 +90,44 @@ const ProductTable = ({ products }) => {
     );
 };
 
+const SearchBar = ({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) => {
+    return (
+        <form>
+            <input type="text"
+                value={filterText}
+                onChange={(e) => onFilterTextChange(e.target.value)}
+                placeholder="Search..."
+            />
+            <label>
+                <input type="checkbox"
+                    checked={inStockOnly}
+                    onChange={(e) => onInStockOnlyChange(e.target.checked)}
+                />
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+};
+
 
 const FilterableProductTable = ({ products }) => {
+
+    const [filterText, setFilterText] = useState("");
+    const [inStockOnly, setInStockOnly] = useState(false);
+
     return (<>
-        {/* <SearchBar /> */}
-        <ProductTable products={products} />
+        <SearchBar
+            filterText={filterText}
+            inStockOnly={inStockOnly}
+            onFilterTextChange={setFilterText}
+            onInStockOnlyChange={setInStockOnly}
+        />
+        <ProductTable
+            products={products}
+            filterText={filterText}
+            inStockOnly={inStockOnly}
+        />
     </>);
 };
 
---
+++ app/react-learn/tic-tac-toe/page.jsx
@@ -0,0 +1,9 @@
+const TicTacToe = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default TicTacToe;
